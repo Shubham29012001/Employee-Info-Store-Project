03b3c174d97a8a71d7052bae03cf9aa0470e3991
Shubham Maurya <smmaurya29012001@gmail.com>
2022-09-08 00:31:21 +0530

    Adding Login Controller Logic and Pagination

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,24 @@
 const employee = require('../models/employee');
-const { badRequestError } = require('../errors/')
+const { badRequestError, unauthenticateError, customAPIError } = require('../errors/')
 
 const loginController = async (req, res) => {
-    res.status(200).send("loginController Route and Controller");
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if (!email || !password) {
+        throw new badRequestError('Please provide both username and password');
+    }
+
+    const user = await employee.findOne({ email });
+    const comparePass = await user.comparePassword(password);
+
+    if (comparePass) {
+        const accessToken = user.createJWT();
+        res.status(200).json({ accessToken, msg: "Login successfully" });
+    }
+    else {
+        throw new unauthenticateError('Please provide correct credentials')
+    }
 }
 
 const signupController = async (req, res) => {
@@ -11,13 +27,25 @@ const signupController = async (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
     const address = req.body.address;
+    const userType = 100;
+    const designation = "";
+    const team = "";
+    const seat = "";
+    const reportingTo = "";
 
     if (!name || !dob || !email || !password || !address) {
         throw new badRequestError("Please provide complete details");;
     }
 
-    const createEmployee = await employee.create({ name, dob, email, password, address });
-    res.status(201).json({ createEmployee, msg: "Employee created successfully" });
+    const employeeEmail = await employee.findOne({ email }).select('-password');
+
+    if (employeeEmail) {
+        throw new customAPIError('User Already Exists');
+    }
+
+    const createEmployee = await employee.create({ name, dob, email, password, address, userType, designation, team, seat, reportingTo });
+    const accessToken = createEmployee.createJWT();
+    res.status(201).json({ createEmployee, accessToken, msg: "Employee created successfully" });
 }
 
 module.exports = {
--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -2,15 +2,21 @@ const employee = require('../models/employee');
 const { badRequestError } = require('../errors/')
 
 const getEmployeeDetails = async (req, res) => {
-    const employees = await employee.find({});
+    const { page } = req.query;
 
-    res.status(200).json({ employees });
+    let limit = 2;
+    let skip = (parseInt(page) - 1) * limit;
+
+    const totalEmployees = await employee.countDocuments({});
+
+    const employees = await employee.find({}).select('-password').limit(limit).skip(skip);
+    res.status(200).json({ employees, totalPages: Math.ceil((totalEmployees / limit)), currentPage: parseInt(page) });
 };
 
 const getIndividualEmployeeDetails = async (req, res) => {
     const id = req.params.id;
 
-    const individualEmployee = await employee.findById({ _id: id });
+    const individualEmployee = await employee.findById({ _id: id }).select('-password');
 
     if (individualEmployee) {
         res.status(200).json({ individualEmployee });
@@ -34,7 +40,6 @@ const deleteIndividualEmployeeDetails = async (req, res) => {
 
 };
 
-
 const updateIndividualEmployeeDetail = async (req, res) => {
     const id = req.params.id;
 
@@ -44,7 +49,7 @@ const updateIndividualEmployeeDetail = async (req, res) => {
         const individualEmployee = await employee.findByIdAndUpdate({ _id: id },
             { name, dob, address, designation, team, seat, reportingTo, email, joiningDate },
             { new: true, runValidators: true }
-        );
+        ).select('-password');
 
         if (individualEmployee) {
             res.status(200).json({ individualEmployee, msg: "Update Successfully" });
